375eac9148b878659856024d28949db0ad37a0be
Jeremy Lee <jeremy.h.lee7@gmail.com>
2018-04-02 12:21:02 -0700

    Shell for location lookup

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 application = Flask(__name__)
 
-@application.route("/")
+@application.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+@application.route("/location", methods=["POST"])
+def lookup():
+    address = request.form["address"]
+    return render_template("location.html", location=address)
 
 if __name__ == "__main__":
     application.run()
