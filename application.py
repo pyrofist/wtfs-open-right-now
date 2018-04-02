1534b0425c15bab6e89097080f4147f89fdccd06
Jeremy Lee <jeremy.h.lee7@gmail.com>
2018-04-02 12:11:45 -0700

    Switch to a template

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 @application.route("/")
 def index():
-    return "<h1>Placeholder</h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     application.run()
